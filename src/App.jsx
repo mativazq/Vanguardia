e896dc23399c16cb654fca80c1d064b2eb7afd12
Mati Vazquez <mativazquezfotografia@hotmail.com>
2025-07-24 22:03:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Header from './Componentes/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>Vanguardia</h1>
+      <h4>Muebles a Medida</h4>
+    </>
+  )
+}
+
+export default Header
