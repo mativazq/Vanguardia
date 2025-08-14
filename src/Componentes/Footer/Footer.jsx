6688d12c67f6a7660064ff919b28b59efaf2311e
Mati Vazquez <mativazquezfotografia@hotmail.com>
2025-08-14 19:08:09 -0300

    nuevo foot

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./footer.css";
+import ImgLogo from "../../Imagenes/Vanguardia-png.png"
 
 const Footer = () => {
   return (
-    <>
-    <h3 className='font-footer'>Footer</h3>
-    </>
-  )
-}
+    <footer className="footer" id="contact">
+      <div className="footer-left">
+      <img src= {ImgLogo} alt="Logo" className="logo" />
+      <a className="foot-by" href="#">Designed by Matias Vazquez</a>
+      </div>
 
-export default Footer
+      <div className="footer-right">
+        <h4>INFO</h4>
+        <ul>
+          <li>
+            <p>TELEFONO:</p>
+            <p>+54 9 11 2345 6789</p>
+          </li>
+          <li>
+            <p>EMAIL:</p>
+            <p>info@vanguardiamuebles.com.ar</p>
+          </li>
+          <li>
+            <p>UBICACION:</p>
+            <p>Santa Fe, Argentina</p>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
