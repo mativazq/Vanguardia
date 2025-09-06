6c45ba4aa20d70d7d4eda7e471a3d9756f1fd366
Mati Vazquez <mativazquezfotografia@hotmail.com>
2025-09-06 15:09:40 -0300

    comienzo bachas

--- src/Componentes/Productos/ListaProd.jsx
+++ src/Componentes/Productos/ListaProd.jsx
@@ -1,62 +1,117 @@
 import bacha1 from '../../Imagenes/img-bacha/bacha-1.jpg'
+import bacha2 from '../../Imagenes/img-bacha/bacha-2.jpg'
+import bacha3 from '../../Imagenes/img-bacha/bacha-3.jpg'
+import bacha4 from '../../Imagenes/img-bacha/bacha-4.jpg'
+import bacha5 from '../../Imagenes/img-bacha/bacha-5.jpg'
+import bacha6 from '../../Imagenes/img-bacha/bacha-6.jpg'
+import bacha7 from '../../Imagenes/img-bacha/bacha-7.jpg'
+import bacha8 from '../../Imagenes/img-bacha/bacha-8.jpg'
+import bacha9 from '../../Imagenes/img-bacha/bacha-9.jpg'
+import bacha10 from '../../Imagenes/img-bacha/bacha-10.jpg'
+import bacha11 from '../../Imagenes/img-bacha/bacha-11.jpg'
+import bacha12 from '../../Imagenes/img-bacha/bacha-12.jpg'
+import bacha13 from '../../Imagenes/img-bacha/bacha-13.jpg'
+import bacha14 from '../../Imagenes/img-bacha/bacha-14.jpg'
+import bacha15 from '../../Imagenes/img-bacha/bacha-15.jpg'
+import bacha16 from '../../Imagenes/img-bacha/bacha-16.jpg'
+import bacha17 from '../../Imagenes/img-bacha/bacha-17.jpg'
+import bacha18 from '../../Imagenes/img-bacha/bacha-18.jpg'
+import bacha19 from '../../Imagenes/img-bacha/bacha-19.jpg'
+import bacha20 from '../../Imagenes/img-bacha/bacha-20.jpg'
+import bacha21 from '../../Imagenes/img-bacha/bacha-21.jpg'
+import bacha22 from '../../Imagenes/img-bacha/bacha-22.jpg'
+import bacha23 from '../../Imagenes/img-bacha/bacha-23.jpg'
+import bacha24 from '../../Imagenes/img-bacha/bacha-24.jpg'
+import bacha25 from '../../Imagenes/img-bacha/bacha-25.jpg'
+import bacha26 from '../../Imagenes/img-bacha/bacha-26.jpg'
+import bacha27 from '../../Imagenes/img-bacha/bacha-27.jpg'
+import bacha28 from '../../Imagenes/img-bacha/bacha-28.jpg'
+import bacha29 from '../../Imagenes/img-bacha/bacha-29.jpg'
+import bacha30 from '../../Imagenes/img-bacha/bacha-30.jpg'
+import bacha31 from '../../Imagenes/img-bacha/bacha-31.jpg'
+import bacha32 from '../../Imagenes/img-bacha/bacha-32.jpg'
+import bacha33 from '../../Imagenes/img-bacha/bacha-33.jpg'
+import bacha34 from '../../Imagenes/img-bacha/bacha-34.jpg'
+import bacha35 from '../../Imagenes/img-bacha/bacha-35.jpg'
+import bacha36 from '../../Imagenes/img-bacha/bacha-36.jpg'
+import bacha37 from '../../Imagenes/img-bacha/bacha-37.jpg'
+import bacha38 from '../../Imagenes/img-bacha/bacha-38.jpg'
+import bacha39 from '../../Imagenes/img-bacha/bacha-39.jpg'
+import bacha40 from '../../Imagenes/img-bacha/bacha-40.jpg'
+import bacha41 from '../../Imagenes/img-bacha/bacha-41.jpg'
+import bacha42 from '../../Imagenes/img-bacha/bacha-42.jpg'
+import bacha43 from '../../Imagenes/img-bacha/bacha-43.jpg'
+import bacha44 from '../../Imagenes/img-bacha/bacha-44.jpg'
+import bacha45 from '../../Imagenes/img-bacha/bacha-45.jpg'
+import bacha46 from '../../Imagenes/img-bacha/bacha-46.jpg'
+import bacha47 from '../../Imagenes/img-bacha/bacha-47.jpg'
+import bacha48 from '../../Imagenes/img-bacha/bacha-48.jpg'
+import bacha49 from '../../Imagenes/img-bacha/bacha-49.jpg'
+import bacha50 from '../../Imagenes/img-bacha/bacha-50.jpg'
+import bacha51 from '../../Imagenes/img-bacha/bacha-51.jpg'
+import bacha52 from '../../Imagenes/img-bacha/bacha-52.jpg'
+import bacha53 from '../../Imagenes/img-bacha/bacha-53.jpg'
+import bacha54 from '../../Imagenes/img-bacha/bacha-54.jpg'
+import bacha55 from '../../Imagenes/img-bacha/bacha-55.jpg'
+
 
 
 export const bachas = [
   { id: 1, nombre: "Signature Enkel 55 Gun Black Nano PVD", medida: "55 x 42 x 22 cm", descripcion: "La pileta Signature Enkel 55 con acabado Gun Black Nano PVD combina elegancia y resistencia. Ideal para cocinas modernas, su innovador recubrimiento ofrece una durabilidad superior y fácil limpieza.", img: bacha1 },
-  { id: 2, nombre: "Signature Enkel 55", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 3, nombre: "Signature Enkel 75 Gun Black Nano PVD", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 4, nombre: "Signature Enkel 75", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 5, nombre: "CURVE SI77 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 6, nombre: "LUXOR SI85 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 7, nombre: "LUXOR COMPACT SI71 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 8, nombre: "LUXOR MINI SI55 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 9, nombre: "QUADRA MAX Q71 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 10, nombre: "QUADRA MINI Q55A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 11, nombre: "QUADRA Q 084 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 12, nombre: "QUADRA Q 085 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 13, nombre: "Quadra Q76", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 14, nombre: "QUADRA Q 76 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 15, nombre: "Quadra Q37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 16, nombre: "Quadra Q40", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 17, nombre: "ZARA D84 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 18, nombre: "E60", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 19, nombre: "E 60 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 20, nombre: "E54", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 21, nombre: "E 50/18", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 22, nombre: "G50", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 23, nombre: "Z 52", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 24, nombre: "ZN 52/18 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 25, nombre: "E 44", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 26, nombre: "E 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 27, nombre: "E 28", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 28, nombre: "T 34", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 29, nombre: "O 37 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 30, nombre: "ON 30A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 31, nombre: "C 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 32, nombre: "C 28", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 33, nombre: "R 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 34, nombre: "R 63", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 35, nombre: "R 63/18 F", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 36, nombre: "Axa D78 A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 37, nombre: "X28", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 38, nombre: "Hydra J 107A", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 39, nombre: "LN 50", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 40, nombre: "DANNA", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 41, nombre: "BALY 340L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 42, nombre: "O 250L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 43, nombre: "O 300L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 44, nombre: "OV 330L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 45, nombre: "O 340L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 46, nombre: "OV 370L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 47, nombre: "OV 440L", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 48, nombre: "EE 28", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 49, nombre: "ZZ 52", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 50, nombre: "CC 28", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 51, nombre: "TT 34", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 52, nombre: "RR 63", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 53, nombre: "RR 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 54, nombre: "CC 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
-  { id: 55, nombre: "EE 37", medida: "", descripcion: "", img: "/img/bacha2.jpg" },
+  { id: 2, nombre: "Signature Enkel 55", medida: "55 x 42 x 22 cm", descripcion: "Compacta y versátil, esta pileta de acero inoxidable 304 con acabado satinado es perfecta para espacios reducidos. Su diseño elegante y funcional garantiza durabilidad y fácil mantenimiento.", img: bacha2 },
+  { id: 3, nombre: "Signature Enkel 75 Gun Black Nano PVD", medida: "75 x 42 x 22 cm", descripcion: "El modelo Signature Enkel 75 con acabado Gun Black Nano PVD ofrece una combinación de capacidad y sofisticación. Su avanzado recubrimiento asegura una resistencia superior a rayaduras y manchas, ideal para cocinas elegantes y funcionales.", img: bacha3 },
+  { id: 4, nombre: "Signature Enkel 75", medida: "75 x 42 x 22 cm", descripcion: "Con mayor capacidad volumétrica, el modelo Signature Enkel 75 con acabado satinado es ideal para quienes necesitan más espacio sin comprometer el estilo. Fabricada en acero inoxidable 304, asegura durabilidad y resistencia.", img: bacha4 },
+  { id: 5, nombre: "CURVE SI77 A", medida: "77 x 43 x 20 cm", descripcion: "Con un diseño elegante y versátil, la pileta CURVE transforma el espacio de trabajo en la cocina. Sus generosas dimensiones, junto con las tablas de vidrio templado y laminado compacto, ofrecen múltiples opciones de uso y máxima funcionalidad. " , img: bacha5 },
+  { id: 6, nombre: "LUXOR SI85 A", medida: "85.2 x 48.2 x 21", descripcion: "Elegancia y funcionalidad se combinan en la Luxor SI85 A, una pileta que aporta distinción a tu cocina.", img: bacha6 },
+  { id: 7, nombre: "LUXOR COMPACT SI71 A", medida: "71.2 x 48.2 x 21", descripcion: "Diseño sofisticado y funcional, la Luxor SI71 A es la opción ideal para cocinas que buscan estilo y eficiencia.", img: bacha7 },
+  { id: 8, nombre: "LUXOR MINI SI55 A", medida: "55.6 x 42.2 x 20", descripcion: "Compacta y sofisticada, la Luxor Mini SI55 A es la elección perfecta para cocinas modernas que buscan eficiencia y estilo.", img: bacha8 },
+  { id: 9, nombre: "QUADRA MAX Q71 A", medida: "71 x 48.2 x 20", descripcion: "Gran capacidad y diseño funcional, la Quadra Max Q71 A es la opción ideal para cocinas que requieren espacio y estilo.", img: bacha9 },
+  { id: 10, nombre: "QUADRA MINI Q55A", medida: "55.5 x 48.9 x 19", descripcion: "Diseño compacto y funcional, la Q55 es la solución perfecta para cocinas modernas que buscan eficiencia y estilo.", img: bacha10 },
+  { id: 11, nombre: "QUADRA Q 084 A", medida: "83.8 x 55.9 x 17.5", descripcion: "Pileta doble de gran capacidad y tamaño. Diseñada para mesadas profundas.De aplicación sobrepuesta, por su elegante borde decorativo. Admite su colocación superior o soldada a una mesada de acero inoxidable. Radio reducido en las esquinas.", img: bacha11 },
+  { id: 12, nombre: "QUADRA Q 085 A", medida: "85.5 x 48.2 x 17.5", descripcion: "Pileta doble de gran capacidad y tamaño. De aplicación sobrepuesta, por su elegante borde decorativo. Admite su colocación inferior o soldada a una mesada de acero inoxidable. Radio de giro reducido en las esquinas casi rectas. Diseño de neto corte purista.", img: bacha12 },
+  { id: 13, nombre: "Quadra Q76", medida: "", descripcion: "", img: bacha13 },
+  { id: 14, nombre: "QUADRA Q 76 A", medida: "", descripcion: "", img: bacha14 },
+  { id: 15, nombre: "Quadra Q37", medida: "", descripcion: "", img: bacha15 },
+  { id: 16, nombre: "Quadra Q40", medida: "", descripcion: "", img: bacha16 },
+  { id: 17, nombre: "ZARA D84 A", medida: "", descripcion: "", img: bacha17 },
+  { id: 18, nombre: "E60", medida: "", descripcion: "", img: bacha18 },
+  { id: 19, nombre: "E 60 A", medida: "", descripcion: "", img: bacha19 },
+  { id: 20, nombre: "E54", medida: "", descripcion: "", img: bacha20 },
+  { id: 21, nombre: "E 50/18", medida: "", descripcion: "", img: bacha21 },
+  { id: 22, nombre: "G50", medida: "", descripcion: "", img: bacha22 },
+  { id: 23, nombre: "Z 52", medida: "", descripcion: "", img: bacha23 },
+  { id: 24, nombre: "ZN 52/18 A", medida: "", descripcion: "", img: bacha24 },
+  { id: 25, nombre: "E 44", medida: "", descripcion: "", img: bacha25 },
+  { id: 26, nombre: "E 37", medida: "", descripcion: "", img: bacha26 },
+  { id: 27, nombre: "E 28", medida: "", descripcion: "", img: bacha27 },
+  { id: 28, nombre: "T 34", medida: "", descripcion: "", img: bacha28 },
+  { id: 29, nombre: "O 37 A", medida: "", descripcion: "", img: bacha29 },
+  { id: 30, nombre: "ON 30A", medida: "", descripcion: "", img: bacha30 },
+  { id: 31, nombre: "C 37", medida: "", descripcion: "", img: bacha31 },
+  { id: 32, nombre: "C 28", medida: "", descripcion: "", img: bacha32 },
+  { id: 33, nombre: "R 37", medida: "", descripcion: "", img: bacha33 },
+  { id: 34, nombre: "R 63", medida: "", descripcion: "", img: bacha34 },
+  { id: 35, nombre: "R 63/18 F", medida: "", descripcion: "", img: bacha35 },
+  { id: 36, nombre: "Axa D78 A", medida: "", descripcion: "", img: bacha36 },
+  { id: 37, nombre: "X28", medida: "", descripcion: "", img: bacha37 },
+  { id: 38, nombre: "Hydra J 107A", medida: "", descripcion: "", img: bacha38 },
+  { id: 39, nombre: "LN 50", medida: "", descripcion: "", img: bacha39 },
+  { id: 40, nombre: "DANNA", medida: "", descripcion: "", img: bacha40 },
+  { id: 41, nombre: "BALY 340L", medida: "", descripcion: "", img: bacha41 },
+  { id: 42, nombre: "O 250L", medida: "", descripcion: "", img: bacha42 },
+  { id: 43, nombre: "O 300L", medida: "", descripcion: "", img: bacha43 },
+  { id: 44, nombre: "OV 330L", medida: "", descripcion: "", img: bacha44 },
+  { id: 45, nombre: "O 340L", medida: "", descripcion: "", img: bacha45 },
+  { id: 46, nombre: "OV 370L", medida: "", descripcion: "", img: bacha46 },
+  { id: 47, nombre: "OV 440L", medida: "", descripcion: "", img: bacha47 },
+  { id: 48, nombre: "EE 28", medida: "", descripcion: "", img: bacha48 },
+  { id: 49, nombre: "ZZ 52", medida: "", descripcion: "", img: bacha49 },
+  { id: 50, nombre: "CC 28", medida: "", descripcion: "", img: bacha50 },
+  { id: 51, nombre: "TT 34", medida: "", descripcion: "", img: bacha51 },
+  { id: 52, nombre: "RR 63", medida: "", descripcion: "", img: bacha52 },
+  { id: 53, nombre: "RR 37", medida: "", descripcion: "", img: bacha53 },
+  { id: 54, nombre: "CC 37", medida: "", descripcion: "", img: bacha54 },
+  { id: 55, nombre: "EE 37", medida: "", descripcion: "", img: bacha55 },
 
 
 ];
